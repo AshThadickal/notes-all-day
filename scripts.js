6cd66217f24032047e0439d5749555b197700d2c
Ashley <ashleythadickal@gmail.com>
2022-04-18 22:25:30 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const app = {}
+
+app.init = () => {
+    app.getQuote()
+}
+
+app.randomizer = (array) => {
+    const index = Math.floor(Math.random() * array.length);
+    const ramdomObj = array[index]
+    return ramdomObj
+}
+
+app.getQuote = () => {
+    $.ajax({
+        url: 'https://type.fit/api/quotes',
+        method: 'GET',
+        dataType: 'JSON'
+    }).then(res => {
+        const theObj = app.randomizer(res)
+        app.displayQuote(theObj)
+    })
+}
+
+app.displayQuote = (quoteObj) => {
+    const quoteDisplay = `<p>${quoteObj.text}</p>
+    <p>Author: ${quoteObj.author}`
+    $('.welcome').prepend(quoteDisplay)
+}
+
+app.init();
